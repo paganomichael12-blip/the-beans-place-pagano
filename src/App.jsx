09db4102dc575408bd082363edc9745d277f7dca
paganomichael12-blip <paganomichael12@gmail.com>
2026-04-29 19:57:19 -0500

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,7 @@ import FeaturesSection from "./components/FeaturesSection";
 import ProductShowcase from "./components/ProductShowcase";
 import CtaSection from "./components/CtaSection";
 import AboutSection from "./components/AboutSection";
+import ContactSection from "./components/ContactSection";
 
 export default function App() {
     return (
@@ -87,16 +88,22 @@ export default function App() {
             <section className="bg-cta">
                 <ProductShowcase />
             </section>
-            
+
             <RibbonTicker />
-            
+
             {/* CTA} */}
             <section className="bg-cta">
                 <CtaSection />
             </section>
-            
+
+            {/* About */}
             <section className="bg-cta" id="about">
                 <AboutSection />
+            </section>
+
+            {/* CONTACT */}
+            <section className="bg-cta" id="contact">
+                <ContactSection />
             </section>
 
             {/* Footer */}
